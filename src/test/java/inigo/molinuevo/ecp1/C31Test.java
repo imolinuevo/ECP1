f79a7cba5e27295c57144d2a6223e0521cfcdd3c
Iñigo Molinuevo <i.m.parron@opendeusto.es>
2015-09-24 17:58:14 +0200

    Crear clase C31Test. Feature.03

---
+++ src/test/java/inigo/molinuevo/ecp1/C31Test.java
@@ -0,0 +1,23 @@
+package inigo.molinuevo.ecp1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class C31Test {
+
+	private C31 c31;
+
+	@Test
+	public void testM1() {
+		c31 = new C31();
+		assertEquals("m1", c31.m1());
+	}
+
+	@Test
+	public void testM2() {
+		c31 = new C31();
+		assertEquals("m2", c31.m2());
+	}
+
+}
