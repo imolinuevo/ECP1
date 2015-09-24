55b937ed3e6c0a1d075a85cf6535ff354c3ccbce
Iñigo Molinuevo <i.m.parron@opendeusto.es>
2015-09-24 17:52:35 +0200

    Crear clase C11Test. Feature.01

---
+++ src/test/java/inigo/molinuevo/ecp1/C11Test.java
@@ -0,0 +1,23 @@
+package inigo.molinuevo.ecp1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class C11Test {
+
+	private C11 c11;
+
+	@Test
+	public void testM1() {
+		c11 = new C11();
+		assertEquals("m1", c11.m1());
+	}
+
+	@Test
+	public void testM2() {
+		c11 = new C11();
+		assertEquals("m2", c11.m2());
+	}
+
+}
